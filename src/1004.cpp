4636eca88016c3e3b942670c12a46905943bc343
Atilio <atiliogoulart@gmail.com>
2021-08-04 13:01:51 -0300

    Done

---
+++ src/1004.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int A;
+    int B;
+    int PROD;
+
+    cin >> A >> B;
+
+    PROD = A*B;
+
+    cout << "PROD = " << PROD << endl;
+
+}
