d9547df08c6052ca0019e68cf54c1e1311694d2b
Atilio <atiliogoulart@gmail.com>
2021-08-06 21:36:39 -0300

    Done

---
+++ src/1016.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <iomanip>
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+
+int main(){
+
+    int a;
+    int resultado;
+
+    cin >> a;
+
+    resultado = (a*2);
+
+    cout << resultado << " minutos" << endl;
+
+
+}
