f8fc1420b4a62bb098f442a55ec4d7704d438468
Hack <atiliogoulart@gmail.com>
2021-08-13 22:35:43 -0300

    Done

---
+++ src/1044.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main(){
+
+    int A,B;
+    cin >> A >> B;
+
+    if (A>B){
+        if (A%B==0){
+            cout << "Sao Multiplos" << endl;
+        }else{
+            cout << "Nao sao Multiplos" << endl;
+        }
+    }else if (B>A){
+        if (B%A==0){
+            cout << "Sao Multiplos" << endl;
+        }else{
+            cout << "Nao sao Multiplos" << endl;
+        }
+    }
+
+}
